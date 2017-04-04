2c17ee1a8679a1faf11676c3ab10c5160e7a5e3e
Shin Park <shinpark@shins-mac-mini.home>
2017-04-04 09:56:34 -0500

    Initial Commit

---
+++ OOP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OOP
+//
+//  Created by Shin Park on 4/4/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
